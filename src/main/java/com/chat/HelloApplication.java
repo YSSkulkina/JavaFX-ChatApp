3cd4abab09b0dc9c6258d333f67c398e7eb332a7
Yulia Skulkina <Yulia.Skulkina@x5.ru>
2025-11-25 17:02:08 +0300

    start

---
+++ src/main/java/com/chat/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.chat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Chat Application");
+        stage.setScene(scene);
+        Image image = new Image(getClass().getResourceAsStream("chat.png"));
+        stage.getIcons().add(image);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.chat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.chat to javafx.fxml;
+    exports com.chat;
+}
